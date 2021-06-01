5554f392a0f6f849fa4692ee90896b8e65e768bc
putchom <putchom@MacBook-Pro.local>
2021-06-02 01:28:45 +0900

    Get images from Tumblr API

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Sizable, dynamic placeholder images featuring Aoi Yagawa
 
 ## Development
 
+1. Copy `.envrc.example` to create `.envrc`.
+2. Enter the Consumer key of the application you created at https://www.tumblr.com/oauth/apps into the `API_KEY` field in `.envrc`.
+3. Execute the following command.
+
 ```
 brew install imagemagick
 bundle install
--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 require 'rmagick'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'nokogiri'
 
 get '/' do
   send_file File.join(File.dirname(__FILE__) + '/public', 'index.html')
@@ -18,9 +22,37 @@ def get_image_filename(width, height)
   filename = File.join(File.dirname(__FILE__) + '/images/generated', "#{width}x#{height}.jpg")
   return filename if FileTest.exist?(filename)
 
-  original_filename = Dir.glob(File.join(File.dirname(__FILE__) + '/images/source', '*.*')).sample
+  original_filename = get_original_image_urls.sample
   image_original = Magick::Image.read(original_filename).first
   image = image_original.resize_to_fill(width, height)
   image.write(filename)
   filename
+end
+
+def get_original_image_urls
+  domain = 'yagawaaoi.tumblr.com'
+  api_key = ENV['API_KEY']
+  json = get_json("https://api.tumblr.com/v2/blog/#{domain}/posts?api_key=#{api_key}")
+  response = json['response']
+  posts = response['posts']
+  original_image_urls = []
+  posts.each{ |post|
+    original_image_urls.push(
+      get_image_src(post['body'])
+    )
+  }
+  original_image_urls
+end
+
+def get_json(location)
+  uri = URI.parse(location)
+  json = Net::HTTP.get(uri)
+  result = JSON.parse(json)
+  result
+end
+
+def get_image_src(body)
+  doc = Nokogiri::HTML(body)
+  img_srcs = doc.css('img').map{ |i| i['src'] }
+  img_src = img_srcs.first
 end
